eb8c140c612617672c5dacdfa0d42e0c58699d0d
XuShunyi <605625197@qq.com>
2019-04-10 11:10:01 +0800

    Update ads-dom-lib.js
    add myLogger function declaration

--- ads-dom-lib.js
+++ ads-dom-lib.js
@@ -14,4 +14,18 @@
   
   window['ADS']['bindFunction'] = bindFunction;
   
+  function myLogger(id) {
+    id = id || 'ADSLogWindow';
+    
+    var logWindow = null;
+    var createWindow = function() {};
+    this.writeRaw = function(message) {};
+  }
+  
+  myLogger.prototype = {
+    write: function (message) {},
+    header: function (message) {},
+    link: function (message) {}
+  };
+  window['ADS']['log'] = new myLogger();
 })();
