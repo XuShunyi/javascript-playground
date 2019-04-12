ed0bdd724e401b5c031f474075aa174e27f877a7
XuShunyi <605625197@qq.com>
2019-04-12 13:40:15 +0800

    Update ads-dom-lib.js
    add addEvent function

--- ads-dom-lib.js
+++ ads-dom-lib.js
@@ -154,4 +154,17 @@
   }
   window['ADS']['preventDefault'] = preventDefault;
   
+  function addEvent (obj, type, fn) {
+    if (obj.attachEvent) {
+      obj['e' + type + fn] = fn;
+      obj[type + fn] = function () {
+        obj['e' + type + fn] (window.event);
+      }
+      obj.attachEvent('on' + type, obj[type + fn]);
+    } else {
+      obj.addEventListener(type, fn, false);
+    }
+  }
+  window['ADS']['addEvent'] = addEvent;
+  
 })();
