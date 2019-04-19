d2b7639813cef49aab4a7b97bf15dcfd286f86e2
XuShunyi <605625197@qq.com>
2019-04-19 10:12:09 +0800

    Update ads-dom-lib.js

--- ads-dom-lib.js
+++ ads-dom-lib.js
@@ -338,4 +338,33 @@
   }
   window['ADS']['getClassNames'] = getClassNames;
   
+  function hasClassName (element, className) {
+    if (!(element = $(element))) return false;
+    let classes = getClassNames(element);
+    for (let i = 0; i < classes.length; i++) {
+      if (classes[i] === className) { return true; }
+    }
+    return false;
+  };
+  window['ADS']['hasClassName'] = hasClassName;
+  
+  function addClassName (element, className) {
+    if (!(element = $(element))) return false;
+    element.className += (element.className ? ' ' : '') + className;
+    return true;
+  };
+  window['ADS']['addClassName'] = addClassName;
+  
+  function removeClassName (element, className) {
+    if (!(element = $(element))) return false;
+    let classes = getClassNames(element);
+    let length = classes.length;
+    for (let i = length -1; i >= 0; i--) {
+      if (classes[i] === className) { delete(classes[i]); }
+    }
+    element.className = classes.join(' ');
+    return (length == classes.length ? false : true);
+  };
+  window['ADS']['removeClassName'] = removeClassName;
+  
 })();
