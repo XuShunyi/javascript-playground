7eea97271f8547470fd8046da58682f4c0534082
XuShunyi <605625197@qq.com>
2019-06-11 09:36:47 +0800

    Update event.js
    add remove function to event object.

--- event.js
+++ event.js
@@ -15,6 +15,22 @@ var event = {
     for (var i = 0, fn; fn = fns[i++]; ) {
       fn.apply(this, arguments);
     }
+  },
+  remove: function (key, fn) {
+    var fns = this.clientList[key];
+    if (!fns) {
+      return false;
+    }
+    if (!fn) {
+      fns && (fns.length = 0);
+    } else {
+      for (var l = fns.length -1; l >= 0; l--) {
+        var _fn = fns[l];
+        if (_fn === fn) {
+          fns.splice(l, 1);
+        }
+      }
+    }
   }
 };
 
